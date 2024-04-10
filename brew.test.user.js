f4d4618487f69b062748cc4f45c19d4660347610
Eguiste <59860367+Eguiste@users.noreply.github.com>
2024-04-11 00:35:15 +0500

    Update brew.test.user.js
    log to console 4

--- brew.test.user.js
+++ brew.test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Brew Page Checker
 // @namespace    https://example.com
-// @version      0.3
+// @version      0.4
 // @description  Outputs 1 to the console when on the brew.sh page.
 // @author       Your name
 // @match        https://brew.sh/*
@@ -14,3 +14,4 @@
 console.log(1)
 console.log(2)
 console.log(3)
+console.log(4)
